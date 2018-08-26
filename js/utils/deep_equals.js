8d6a39c592f62e93385d43f901cd249a1382e18f
James Hines <jameshines10@gmail.com>
2018-08-26 15:53:56 -0500

    Adds tests for deep equality

--- js/utils/deep_equals.js
+++ js/utils/deep_equals.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const isEqual = require('../js/utils/is_equal');
+const isEqual = require('./is_equal');
 
 module.exports = (a, b) => {
   return isEqual(a.x, b.x) && isEqual(a.y, b.y) && isEqual(a.z, b.z);
--- test/deep.equals.spec.js
+++ test/deep.equals.spec.js
@@ -7,11 +7,18 @@ const Vector = require('../js/vector');
 
 describe('Utility method for determining if two objects are equal', () => {
   describe('deepEquals returns true if the values for the x, y, and z properties of two points are equal ', () => {
-    it('', () => {
+    it('should return true if pointA and pointB have the same values', () => {
       let pointA = new Point(3.5, 2.5, 4.5);
       let pointB = new Point(3.5, 2.5, 4.5);
 
       expect(deepEquals(pointA, pointB)).to.equal(true);
+    });
+
+    it('should return false if vectorA and vectorB have different values', () => {
+      let vectorA = new Vector(3.5, 2.5, 4.5);
+      let vectorB = new Vector(3.6, 2.6, 4.5);
+
+      expect(deepEquals(vectorA, vectorB)).to.equal(false);
     });
   });
 });
