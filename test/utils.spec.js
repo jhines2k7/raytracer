39d4350f67d3952b74ba74389aacaa3426d8b1f2
James Hines <jameshines10@gmail.com>
2018-08-26 10:52:35 -0500

    Renames unit tests

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -19,14 +19,14 @@ describe('Utility method for comparing floating point numbers', () => {
       expect(equal(a, b)).to.equal(true);
     });
 
-    it('are considered not equal if their difference is less than 0.00001 when a = 2.12345 and b = 2.12346', () => {
+    it('are considered not equal if their difference is greater than 0.00001 when a = 2.12345 and b = 2.12346', () => {
       let a = 2.12345;
       let b = 2.12346;
 
       expect(equal(a, b)).to.equal(false);
     });
 
-    it('are considered not equal if their difference is greater than 0.00001', () => {
+    it('are considered not equal if their difference is greater than 0.00001 when a = 2.12345 and b = 2.12356', () => {
       let a = 1.12345;
       let b = 1.12356;
 
