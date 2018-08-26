4b2159c9d19459abbc482c273a9d7191210c6c52
James Hines <jameshines10@gmail.com>
2018-08-26 10:54:53 -0500

    Renames equality utility file

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const expect = require('chai').expect;
-const equal = require('../js/utils/equal');
+const isEqual = require('../js/utils/is_equal');
 
 describe('Utility method for comparing floating point numbers', () => {
   describe('Two floating point numbers', () => {
@@ -9,28 +9,28 @@ describe('Utility method for comparing floating point numbers', () => {
       let a = 2.12345;
       let b = 2.12345;
 
-      expect(equal(a, b)).to.equal(true);
+      expect(isEqual(a, b)).to.equal(true);
     });
 
     it('are considered equal if their difference is less than 0.00001 when a = 2.1234566 and b = 2.1234567', () => {
       let a = 2.1234566;
       let b = 2.1234567;
 
-      expect(equal(a, b)).to.equal(true);
+      expect(isEqual(a, b)).to.equal(true);
     });
 
     it('are considered not equal if their difference is greater than 0.00001 when a = 2.12345 and b = 2.12346', () => {
       let a = 2.12345;
       let b = 2.12346;
 
-      expect(equal(a, b)).to.equal(false);
+      expect(isEqual(a, b)).to.equal(false);
     });
 
     it('are considered not equal if their difference is greater than 0.00001 when a = 2.12345 and b = 2.12356', () => {
       let a = 1.12345;
       let b = 1.12356;
 
-      expect(equal(a, b)).to.equal(false);
+      expect(isEqual(a, b)).to.equal(false);
     });
   })
 });
