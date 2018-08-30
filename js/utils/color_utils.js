270fcd6a6a8a24c95dfce263dfa9594452457478
James Hines <jameshines10@gmail.com>
2018-08-30 11:52:12 -0400

    Adds methods and tests for multiplying colors by scalars and finding the hadamard product of two colors

--- js/utils/color_utils.js
+++ js/utils/color_utils.js
@@ -10,4 +10,12 @@ function subtractColors(colorA, colorB) {
   return new Color(colorA.red - colorB.red, colorA.green - colorB.green, colorA.blue - colorB.blue);
 }
 
-module.exports = {addColors, subtractColors};
+function multiplyByScalar(scalar, color) {
+  return new Color(scalar * color.red, scalar * color.green, scalar * color.blue);
+}
+
+function hadamardProduct(colorA, colorB) {
+ return new Color(colorA.red * colorB.red, colorA.green * colorB.green, colorA.blue * colorB.blue);
+}
+
+module.exports = {addColors, subtractColors, multiplyByScalar, hadamardProduct};
--- test/color_utils.spec.js
+++ test/color_utils.spec.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const expect = require('chai').expect;
-const addColors = require('../js/utils/color_utils').addColors;
-const subtractColors = require('../js/utils/color_utils').subtractColors;
+const colorUtils = require('../js/utils/color_utils');
+const addColors = colorUtils.addColors;
+const subtractColors = colorUtils.subtractColors;
+const multiplyByScalar = colorUtils.multiplyByScalar;
+const hadamardProduct = colorUtils.hadamardProduct;
 const Color = require('../js/color');
 
 describe('Adding colors', () => {
@@ -31,8 +34,40 @@ describe('Subtracting colors', () => {
   it('given a color (0.9, 0.6, 0.75) and a color (0.7, 0.1, 0.25) subtractColors should return a color (0.2, 0.5, 0.5)', () => {
     let color = subtractColors(new Color(0.9, 0.6, 0.75), new Color(0.7, 0.1, 0.25));
 
-    expect(color.red).to.equal(0.2);
+    expect(parseFloat(color.red.toFixed(1))).to.equal(0.2);
     expect(color.green).to.equal(0.5);
     expect(color.blue).to.equal(0.5);
+  });
+});
+
+describe('Multiplying by scalar colors', () => {
+  it('subtractColors should return a color', () => {
+    let color = multiplyByScalar(2, new Color(0.9, 0.6, 0.75));
+
+    expect(color.constructor.name).to.equal('Color');
+  });
+
+  it('given a color (0.2, 0.3, 0.4) and a scalar multiplyByScalar should return a color (0.4, 0.6, 0.8)', () => {
+    let color = multiplyByScalar(2, new Color(0.2, 0.3, 0.4));
+
+    expect(color.red).to.equal(0.4);
+    expect(color.green).to.equal(0.6);
+    expect(color.blue).to.equal(0.8);
+  });
+});
+
+describe('Hadamard prodcut', () => {
+  it('hadamardProduct should return a color', () => {
+    let color = hadamardProduct(new Color(1, 0.2, 0.4), new Color(0.9, 1, 0.1));
+
+    expect(color.constructor.name).to.equal('Color');
+  });
+
+  it('given a color (1, 0.2, 0.4) and a color (0.9, 1, 0.1) hadamardProdcut should return a color (0.9, 0.2, 0.04)', () => {
+    let color = hadamardProduct(new Color(1, 0.2, 0.4), new Color(0.9, 1, 0.1));
+
+    expect(color.red).to.equal(0.9);
+    expect(color.green).to.equal(0.2);
+    expect(parseFloat(color.blue.toFixed(2))).to.equal(0.04);
   });
 });
