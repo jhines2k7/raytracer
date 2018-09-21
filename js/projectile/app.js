d7901c696c64250cfcc0987333dab51c9de4d175
James Hines <jameshines10@gmail.com>
2018-09-20 19:53:29 -0500

    Adds logic to draw te path of a projectile onto the canvas

--- js/projectile/app.js
+++ js/projectile/app.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 
 const normalize = require('../utils/normalize');
+const multiplyByScalar = require('../utils/scalar_multiplication');
 
 const Simulator = require('./simulator');
 const World = require('./world');
@@ -17,7 +18,7 @@ let wind = new Vector(-0.01, 0, 0);
 let world = new World(gravity, wind);
 
 let start = new Point(0, 1, 0);
-let velocity = normalize(new Vector(1, 1.8, 0)) * 11.25;
+let velocity = multiplyByScalar(11.25, normalize(new Vector(1, 1.8, 0)));
 let projectile = new Projectile(start, velocity);
 
 let simulator = new Simulator();
@@ -26,23 +27,41 @@ let canvas = new Canvas(900, 550, new Color(0, 0, 0));
 
 let simulatedProjectile = simulator.tick(world, projectile);
 
+const GREEN = new Color(0, 1, 0);
+const RED = new Color(1, 0, 0);
+const BLUE = new Color(0, 0, 1);
+
+let canvasX = 0, canvasY = 0;
+
 while(simulatedProjectile.position.y >= 0) {
   simulatedProjectile = simulator.tick(world, simulatedProjectile);
+
+  canvasX = Math.ceil(simulatedProjectile.position.x);
+  canvasY = canvas.height - Math.ceil(simulatedProjectile.position.y);
+
+  canvas.writePixel(canvasX, canvasY, GREEN);
+  canvas.writePixel(canvasX - 1, canvasY, GREEN);
+  canvas.writePixel(canvasX + 1, canvasY, GREEN);
+  canvas.writePixel(canvasX, canvasY - 1, GREEN);
+  canvas.writePixel(canvasX, canvasY + 1, GREEN);
 }
 
 let FILE_PATH = './';
 let FILE_NAME = 'projectile.ppm';
+let FILE = FILE_PATH + FILE_NAME;
 
-fs.unlink(FILE_PATH + FILE_NAME, (err) => {
-  if(err) throw err;
+if(fs.existsSync(FILE)) {
+  fs.unlink(FILE, (err) => {
+    if(err) throw err;
 
-  console.log('ppm file was deleted');
-});
+    console.log('ppm file was deleted');
+  });
+}
 
-fs.writeFile(FILE_PATH + FILE_NAME, canvas.canvasToPpm(), (err) => {
+fs.writeFile(FILE, canvas.canvasToPpm(), (err) => {
   if(err) {
     return console.log(err);
   }
 
-  console.log('ppm file was saved successfully!');
+  console.log('ppm file was created and saved successfully!');
 });
