c8984187927393b9b0a593d4fdf4ab5caa301990
James Hines <jameshines10@gmail.com>
2018-11-23 17:26:53 -0600

    Finishes the program to render the silhouette of a sphere

--- js/putting_it_together/chapter5.js
+++ js/putting_it_together/chapter5.js
@@ -3,28 +3,46 @@
 const fs = require('fs');
 
 const Point = require('../point');
-const Vector = require('../vector');
 const Color = require('../color');
 const Canvas = require('../canvas');
 const Sphere = require('../sphere');
 const Ray = require('../ray');
-const transformations = require('../utils/transformations');
-const translation = transformations.translation;
-const rotationZ = transformations.rotationZ;
-const scaling = transformations.scaling;
-const transform = transformations.transform;
-const matrixMultiply = require('../utils/matrix_utils').matrixMultiply;
+const normalize = require('../utils/normalize');
+const subtract = require('../utils/subtraction');
+const intersectionUtils = require('../utils/intersection_utils');
+const intersect = intersectionUtils.intersect;
+const hit = intersectionUtils.hit;
 
-let canvas = new Canvas(900, 900, new Color(0, 0, 0));
+let rayOrigin = new Point(0, 0, -5);
+let wallZ = 10;
+let wallSize = 7;
+let canvasPixels = 500;
+let pixelSize = wallSize / canvasPixels;
+let half = wallSize / 2;
+let canvas = new Canvas(canvasPixels, canvasPixels, new Color(0, 0, 0));
 let sphere = new Sphere();
-sphere.transform = scaling(2, 2, 2);
 
 const RED = new Color(1, 0, 0);
 
 // cast a ray at each pixel on the canvas
-for(let row = 0; row < canvas.width; row++) {
-  for(let column = 0; column < canvas.height; column++) {
-    let ray = new Ray(new Point(row, column, 0), new Vector(0, 0, 1));
+for(let y = 0; y < canvasPixels; y++) {
+  // compute the world y coordinate (top = +half, bottom = -half)
+  let worldY = half - pixelSize * y;
+
+  for(let x = 0; x < canvasPixels; x++) {
+    // compute the world x coordinate (left = -half, right = half)
+    let worldX = (half * -1) + pixelSize * x;
+
+    // describe the point on the wall the ray will target
+    let position = new Point(worldX, worldY, wallZ);
+
+    let ray = new Ray(rayOrigin, normalize(subtract(position, rayOrigin)));
+
+    let intersections = intersect(sphere, ray);
+
+    if(hit(intersections)) {
+      canvas.writePixel(x, y, RED);
+    }
   }
 }
 
