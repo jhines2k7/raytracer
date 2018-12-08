fd383f1a95528462c34db84ef9ddb91715ac956c
James Hines <jameshines10@gmail.com>
2018-12-07 19:08:54 -0600

    Completes the exercise in chapter 6 putting it together

--- js/putting_it_together/chapter6.js
+++ js/putting_it_together/chapter6.js
@@ -12,17 +12,25 @@ const subtract = require('../utils/subtraction');
 const intersectionUtils = require('../utils/intersection_utils');
 const intersect = intersectionUtils.intersect;
 const hit = intersectionUtils.hit;
+const position = intersectionUtils.position;
+const PointLight = require('../point_light');
+const lightShadingUtils = require('../utils/light_shading_utils');
+const normalAt = lightShadingUtils.normalAt;
+const lighting = lightShadingUtils.lighting;
+const negate = require('../utils/negate');
 
 let rayOrigin = new Point(0, 0, -5);
 let wallZ = 10;
 let wallSize = 7;
-let canvasPixels = 500;
+let canvasPixels = 1500;
 let pixelSize = wallSize / canvasPixels;
 let half = wallSize / 2;
 let canvas = new Canvas(canvasPixels, canvasPixels, new Color(0, 0, 0));
 let sphere = new Sphere();
+sphere.material.color = new Color(0, 0, 1);
+sphere.material.shininess = 50;
 
-const RED = new Color(1, 0, 0);
+let pointLight = new PointLight(new Point(-10, 10, -10), new Color(1, 1, 1));
 
 // cast a ray at each pixel on the canvas
 for(let y = 0; y < canvasPixels; y++) {
@@ -34,20 +42,28 @@ for(let y = 0; y < canvasPixels; y++) {
     let worldX = (half * -1) + pixelSize * x;
 
     // describe the point on the wall the ray will target
-    let position = new Point(worldX, worldY, wallZ);
+    let pointOnWall = new Point(worldX, worldY, wallZ);
 
-    let ray = new Ray(rayOrigin, normalize(subtract(position, rayOrigin)));
+    let ray = new Ray(rayOrigin, normalize(subtract(pointOnWall, rayOrigin)));
 
     let intersections = intersect(sphere, ray);
 
     if(hit(intersections)) {
-      canvas.writePixel(x, y, RED);
+      let intersection = hit(intersections);
+
+      let point = position(ray, intersection.timeValueOfIntersection);
+      let normal = normalAt(intersection.intersectedObject, point);
+      let eyeVector = negate(ray.direction);
+
+      let color = lighting(intersection.intersectedObject.material, pointLight, point, eyeVector, normal);
+
+      canvas.writePixel(x, y, color);
     }
   }
 }
 
 let FILE_PATH = './';
-let FILE_NAME = 'sphere_shadow.ppm';
+let FILE_NAME = 'raytraced_sphere.ppm';
 let FILE = FILE_PATH + FILE_NAME;
 
 if(fs.existsSync(FILE)) {
